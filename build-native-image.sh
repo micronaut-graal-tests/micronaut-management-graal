77d8c6d6dd57a4d2e5f246927686225bb0c38ae8
Iván López <lopez.ivan@gmail.com>
2019-05-02 16:36:33 +0200

    Use Micronaut 1.1.1-graal.BUILD-SNAPSHOT that removes the deprecated flags

--- build-native-image.sh
+++ build-native-image.sh
@@ -1,2 +1,2 @@
 ./gradlew assemble
-native-image --no-server --no-fallback --class-path build/libs/management-*.jar --initialize-at-build-time=io.micrometer.core
+native-image --no-server --no-fallback --class-path build/libs/management-*.jar
